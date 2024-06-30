a6069694690cfdf301b6f18f0658c8c66eade62b
Max <maxmalvin@hotmail.com>
2024-06-30 22:43:29 +0200

    Initial Setup

---
+++ CadCamMachining.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using CadCamMachining.Client;
+using CadCamMachining.Client.Services.Contracts;
+using CadCamMachining.Client.Services.Implementations;
+using CadCamMachining.Client.States;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
